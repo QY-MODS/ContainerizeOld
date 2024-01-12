c55349adfd4ba5688e6f1b0525cab86f057caed0
Quantumyilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2024-01-12 15:14:25 +0100

    _

--- Utils.h
+++ Utils.h
@@ -2,3 +2,258 @@
 
 //#include <chrono>
 #include "logger.h"
+#include <windows.h>
+#include <functional>
+
+namespace Utilities {
+
+    const auto mod_name = static_cast<std::string>(SKSE::PluginDeclaration::GetSingleton()->GetName());
+    constexpr auto path = L"Data/SKSE/Plugins/ContainableFramework.ini";
+    constexpr auto po3path = "Data/SKSE/Plugins/po3_Tweaks.dll";
+
+    const auto no_src_msgbox = std::format(
+        "{}: You currently do not have any container set up. Check your ini file or see the mod page for instructions.",
+        mod_name);
+    const auto po3_err_msgbox = std::format(
+        "{}: You have given an invalid FormID. If you are using Editor IDs, you must have powerofthree's Tweaks "
+        "installed. See mod page for further instructions.",
+        mod_name);
+    const auto general_err_msgbox = std::format("{}: Something went wrong. Please contact the mod author.", mod_name);
+    const auto init_err_msgbox = std::format("{}: The mod failed to initialize and will be terminated.", mod_name);
+    /*const auto load_order_msgbox = std::format(
+        "The equipped light source from your save game could not be registered. Please unequip and reequip it. If you "
+        "had fuel in it, it will be lost. This issue will be solved in the next version.");*/
+
+    template <typename T>
+    std::string join(const T& container, const std::string_view& delimiter) {
+        std::ostringstream oss;
+        auto iter = container.begin();
+
+        if (iter != container.end()) {
+            oss << *iter;
+            ++iter;
+        }
+
+        for (; iter != container.end(); ++iter) {
+            oss << delimiter << *iter;
+        }
+
+        return oss.str();
+    }
+
+    std::string DecodeTypeCode(std::uint32_t typeCode) {
+        char buf[4];
+        buf[3] = char(typeCode);
+        buf[2] = char(typeCode >> 8);
+        buf[1] = char(typeCode >> 16);
+        buf[0] = char(typeCode >> 24);
+        return std::string(buf, buf + 4);
+    }
+
+    std::string dec2hex(int dec) {
+        std::stringstream stream;
+        stream << std::hex << dec;
+        std::string hexString = stream.str();
+        return hexString;
+    };
+
+    bool isValidHexWithLength7or8(const char* input) {
+        std::string inputStr(input);
+        std::regex hexRegex("^[0-9A-Fa-f]{7,8}$");  // Allow 7 to 8 characters
+        bool isValid = std::regex_match(inputStr, hexRegex);
+        return isValid;
+    }
+
+    float Round(float number, int decimalPlaces) {
+        auto rounded_number =
+            static_cast<float>(std::round(number * std::pow(10, decimalPlaces))) / std::pow(10, decimalPlaces);
+        return rounded_number;
+    }
+
+    int Round2Int(float number) {
+        auto rounded_number = static_cast<int>(Round(number, 0));
+        return rounded_number;
+    }
+
+    bool IsPo3Installed() { return std::filesystem::exists(po3path); };
+
+    namespace MsgBoxesNotifs {
+
+        namespace Windows {
+
+            int Po3ErrMsg() {
+                MessageBoxA(nullptr, po3_err_msgbox.c_str(), "Error", MB_OK | MB_ICONERROR);
+                return 1;
+            };
+
+            int GeneralErr() {
+                MessageBoxA(nullptr, general_err_msgbox.c_str(), "Error", MB_OK | MB_ICONERROR);
+                return 1;
+            };
+        };
+
+        namespace InGame {
+
+            void InitErr() { RE::DebugMessageBox(init_err_msgbox.c_str()); };
+
+            void GeneralErr() { RE::DebugMessageBox(general_err_msgbox.c_str()); };
+
+            void NoSourceFound() { RE::DebugMessageBox(no_src_msgbox.c_str()); };
+
+            void FormIDError(RE::FormID id) {
+                RE::DebugMessageBox(
+                    std::format("{}: The ID ({}) you have provided in the ini file could not have been found.",
+                                Utilities::mod_name, Utilities::dec2hex(id))
+                        .c_str());
+            }
+
+            void EditorIDError(std::string id) {
+                RE::DebugMessageBox(
+                    std::format("{}: The ID ({}) you have provided in the ini file could not have been found.",
+                                Utilities::mod_name, id)
+                        .c_str());
+            }
+
+            /*void LoadOrderError() {
+                RE::DebugMessageBox((std::format("{}: ", Utilities::mod_name) + load_order_msgbox).c_str());
+            }*/
+        };
+    };
+
+    // Custom struct to represent the combined data
+    // butun RE::FormID'ler bununla takas edildi
+    struct FormID2 {
+        uint32_t outerKey;
+        uint32_t innerKey;
+
+        // Comparison operator for using Key as std::map key
+        bool operator<(const FormID2& other) const {
+            return outerKey < other.outerKey || (outerKey == other.outerKey && innerKey < other.innerKey);
+        }
+    };
+
+    // https :  // github.com/ozooma10/OSLAroused/blob/29ac62f220fadc63c829f6933e04be429d4f96b0/src/PersistedData.cpp
+    template <typename T>
+    // BaseData is based off how powerof3's did it in Afterlife
+    class BaseData {
+    public:
+        float GetData(FormID2 formId, T missing) {
+            Locker locker(m_Lock);
+            if (auto idx = m_Data.find(formId) != m_Data.end()) {
+                return m_Data[formId];
+            }
+            return missing;
+        }
+
+        void SetData(FormID2 formId, T value) {
+            Locker locker(m_Lock);
+            m_Data[formId] = value;
+        }
+
+        virtual const char* GetType() = 0;
+
+        virtual bool Save(SKSE::SerializationInterface* serializationInterface, std::uint32_t type,
+                          std::uint32_t version);
+        virtual bool Save(SKSE::SerializationInterface* serializationInterface);
+        virtual bool Load(SKSE::SerializationInterface* serializationInterface);
+
+        void Clear();
+
+        virtual void DumpToLog() = 0;
+
+    protected:
+        std::map<FormID2, T> m_Data;
+
+        using Lock = std::recursive_mutex;
+        using Locker = std::lock_guard<Lock>;
+        mutable Lock m_Lock;
+    };
+
+    class BaseFormFloat : public BaseData<float> {
+    public:
+        virtual void DumpToLog() override {
+            Locker locker(m_Lock);
+            for (const auto& [formId, value] : m_Data) {
+                logger::info("Dump Row From {} - FormID1st: {} - FormID2nd: {} - value: {}", GetType(), formId.outerKey,
+                             formId.innerKey, value);
+            }
+            // sakat olabilir
+            logger::info("{} Rows Dumped For Type {}", m_Data.size(), GetType());
+        }
+    };
+
+    // BaseData is based off how powerof3's did it in Afterlife
+    template <typename T>
+    bool BaseData<T>::Save(SKSE::SerializationInterface* serializationInterface, std::uint32_t type,
+                           std::uint32_t version) {
+        if (!serializationInterface->OpenRecord(type, version)) {
+            logger::error("Failed to open record for Data Serialization!");
+            return false;
+        }
+
+        return Save(serializationInterface);
+    }
+
+    template <typename T>
+    bool BaseData<T>::Save(SKSE::SerializationInterface* serializationInterface) {
+        assert(serializationInterface);
+        Locker locker(m_Lock);
+
+        const auto numRecords = m_Data.size();
+        if (!serializationInterface->WriteRecordData(numRecords)) {
+            logger::error("Failed to save {} data records", numRecords);
+            return false;
+        }
+
+        for (const auto& [formId, value] : m_Data) {
+            if (!serializationInterface->WriteRecordData(formId)) {
+                logger::error("Failed to save data for FormID2: ({},{})", formId.outerKey, formId.innerKey);
+                return false;
+            }
+
+            if (!serializationInterface->WriteRecordData(value)) {
+                logger::error("Failed to save value data for form2: ({},{})", formId.outerKey, formId.innerKey);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    template <typename T>
+    bool BaseData<T>::Load(SKSE::SerializationInterface* serializationInterface) {
+        assert(serializationInterface);
+
+        std::size_t recordDataSize;
+        serializationInterface->ReadRecordData(recordDataSize);
+
+        Locker locker(m_Lock);
+        m_Data.clear();
+
+        FormID2 formId;
+        T value;
+
+        for (auto i = 0; i < recordDataSize; i++) {
+            serializationInterface->ReadRecordData(formId);
+            // Ensure form still exists
+            // bunu nasil yapacagiz?
+            FormID2 fixedId;
+            if (!serializationInterface->ResolveFormID(formId.outerKey, fixedId.outerKey) ||
+                !serializationInterface->ResolveFormID(formId.innerKey, fixedId.innerKey)) {
+                logger::error("Failed to resolve formID1st {} {} and Failed to resolve formID2nd {} {}"sv,
+                              formId.outerKey, fixedId.outerKey, formId.innerKey, fixedId.innerKey);
+                continue;
+            }
+
+            serializationInterface->ReadRecordData(value);
+            m_Data[formId] = value;
+        }
+        return true;
+    }
+
+    template <typename T>
+    void BaseData<T>::Clear() {
+        Locker locker(m_Lock);
+        m_Data.clear();
+    }
+
+};
--- plugin.cpp
+++ plugin.cpp
@@ -1,5 +1,12 @@
 #include "Manager.h"
 
+
+RE::TESObjectCONT* unownedChest = nullptr;
+RE::TESObjectREFR* unownedChestObjRefr = nullptr;
+RE::TESObjectCELL* unownedCell = nullptr;
+RE::NiPoint3 unownedChestPos = { 1989.0740f, 1793.2015f, 6784.0000f };
+uint32_t total_chests = 1;
+
 bool EqStr(const char* str1, const char* str2) { return std::strcmp(str1, str2) == 0; }
 
 class OurEventSink : public RE::BSTEventSink<RE::TESHitEvent>,
@@ -29,10 +36,31 @@ public:
 
     RE::BSEventNotifyControl ProcessEvent(const RE::TESActivateEvent* event,
                                           RE::BSTEventSource<RE::TESActivateEvent>*) {
+        if (!event) return RE::BSEventNotifyControl::kContinue;
+        if (!event->objectActivated) return RE::BSEventNotifyControl::kContinue;
         auto activatedName = event->objectActivated->GetBaseObject()->GetName();
-        /*if (EqStr(activatedName,"Unowned Chest")) {
-            logger::info("Unowned Chest is activatable: {}", event->objectActivated->IsActivationBlocked());
-		}*/
+        if (EqStr(activatedName,"Unowned Chest")) {
+            logger::info("Unowned Chest is not activatable: {}", event->objectActivated->IsActivationBlocked());
+            auto item = event->objectActivated->GetBaseObject();
+            /*unownedChest->AddObjectToContainer(item,10,RE::PlayerCharacter::GetSingleton()->As<RE::TESForm>());
+            unownedChest->fullName = "lol";
+            unownedChest->Activate(event->objectActivated.get(), RE::PlayerCharacter::GetSingleton(), 0, item, 10);*/
+            total_chests += 1;
+            logger::info("asd");
+            logger::info("total_chests {}", total_chests);
+            int total_chests_x = ((((int)total_chests - 1) + 2) % 5) - 2;
+            logger::info("total_chests_x: {}", total_chests_x);
+            //unownedChestPos.y += 50.0f * ((total_chests - 1));
+            float Pos3_x = unownedChestPos.x + 100.0f * total_chests_x;
+            RE::NiPoint3 Pos3 = { Pos3_x, unownedChestPos.y, unownedChestPos.z };
+            logger::info("Pos3: {}, {}, {}", Pos3.x, Pos3.y, Pos3.z);
+            auto newPropRef = RE::TESDataHandler::GetSingleton()->CreateReferenceAtLocation(item, Pos3,
+                                                                                  {0.0f, 0.0f, 0.0f},
+                                                              unownedCell, nullptr, nullptr, nullptr,
+                                                              {}, true, false).get().get();
+            logger::info("Created Object! Type: {}, Base ID: {:x}, Ref ID: {:x},",
+                         RE::FormTypeToString(item->GetFormType()), item->GetFormID(), newPropRef->GetFormID());
+		}
         auto activatorName = event->actionRef->GetBaseObject()->GetName();
         logger::info("{} activated {}", activatorName, activatedName);
         return RE::BSEventNotifyControl::kContinue;
@@ -40,12 +68,11 @@ public:
 
     RE::BSEventNotifyControl ProcessEvent(const SKSE::CrosshairRefEvent* event,
                                           RE::BSTEventSource<SKSE::CrosshairRefEvent>*) {
-        if (event->crosshairRef) {
-            logger::info("Crosshair is over {}", event->crosshairRef->GetBaseObject()->GetName());
-        }
-        /*if (EqStr(event->crosshairRef->GetBaseObject()->GetName(), "Unowned Chest")) {
+        if (!event->crosshairRef) return RE::BSEventNotifyControl::kContinue;
+        logger::info("Crosshair is over {}", event->crosshairRef->GetBaseObject()->GetName());
+        if (EqStr(event->crosshairRef->GetBaseObject()->GetName(), "Unowned Chest")) {
             event->crosshairRef->SetActivationBlocked(1);
-        }*/
+        }
         return RE::BSEventNotifyControl::kContinue;
     }
 
@@ -76,8 +103,33 @@ public:
 
 
 void OnMessage(SKSE::MessagingInterface::Message* message) {
-    if (message->type == SKSE::MessagingInterface::kInputLoaded)
-        RE::BSInputDeviceManager::GetSingleton()->AddEventSink(OurEventSink::GetSingleton());
+    /*if (message->type == SKSE::MessagingInterface::kInputLoaded)
+        RE::BSInputDeviceManager::GetSingleton()->AddEventSink(OurEventSink::GetSingleton());*/
+    if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+        unownedChest = RE::TESForm::LookupByID<RE::TESObjectCONT>(0x000EA299);
+        if (!unownedChest) logger::info("Unowned Chest not found");
+        else {
+            logger::info("Unowned Chest found {}", unownedChest->GetName());
+            unownedChestObjRefr = unownedChest->As<RE::TESObjectREFR>();
+            //auto unownedChest3D = unownedChestObjRefr->Get3D();
+        }
+
+        unownedCell = RE::TESForm::LookupByID<RE::TESObjectCELL>(0x000EA28B);
+        if (!unownedCell) logger::info("Unowned Cell not found");
+        else {
+            logger::info("Unowned Cell found {}", unownedCell->GetName());
+            auto runtimeData = unownedCell->GetRuntimeData();
+            for (const auto& ref : unownedCell->GetRuntimeData().references) {
+                logger::info("Girdik");
+                if (!ref) logger::info("Null object in cell");
+                else {
+                    logger::info("Object in cell: {}", ref->GetBaseObject()->GetName());
+                    logger::info("Object in cell: {}", ref->GetBaseObject()->GetFormEditorID());
+                }
+			}
+        }
+
+    }
 }
 
 
@@ -99,8 +151,7 @@ SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     // UI
     RE::UI::GetSingleton()->AddEventSink<RE::MenuOpenCloseEvent>(eventSink);
 
-    // Input Device
-    //SKSE::GetMessagingInterface()->RegisterListener(OnMessage);
+    SKSE::GetMessagingInterface()->RegisterListener(OnMessage);
 
     return true;
 }
